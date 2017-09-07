73628ca1ee458cc16d951b8c3370d5173b4fbc69
Ravish Rawal <rr2914@columbia.edu>
2017-09-07 19:39:35 -0400

    more ajax

--- routes/offices.js
+++ routes/offices.js
@@ -1,7 +1,19 @@
+const db = require('../db')
+const User = require('../db/User.js')
+const Office = require('../db/Office.js')
 const router = require('express').Router();
 module.exports= router;
 
-router.post('/', (req, res, next)=>{
-  console.log(req.body);
-  res.send(req.body)
+router.get('/', function(req, res, next){
+  Office.findAll()
+    .then((offices)=>{
+      res.send(offices);
+    })
+})
+
+router.post('/', function(req, res, next){
+  Office.create({ name: req.body.newLocation })
+    .then(()=>{
+      res.redirect('/');
+    })
 })
